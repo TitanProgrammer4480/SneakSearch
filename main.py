214827f9937014e0054eb2fa93a098f7a6b8cf92
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-03 09:25:10 +0000

    .

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from functions.google import g_search
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        term = request.form["term"]
+        result = g_search(term, 10, "us")
+        return render_template("home.html", web_res=result)
     return render_template("home.html")
 
 if __name__ == '__main__':
