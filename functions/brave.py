9718b83d327e450630642af24c4cfdf2bbaceae3
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-04 16:55:04 +0000

    added brave

---
+++ functions/brave.py
@@ -0,0 +1,15 @@
+import requests as r
+from bs4 import BeautifulSoup as bs
+
+
+def brave_search(s_term):
+  s_results = []
+  results = r.get(f"https://search.brave.com/search?q={s_term}&offset=0")
+  soup = bs(results.text, 'html.parser')
+  divs = soup.find_all("div", attrs={"data-type": "web"}, class_="snippet svelte-wmqx28")
+  for i in divs:
+    url = i.find("a", class_="h svelte-8pffig l1")["href"]
+    title = i.find("div", class_="sitename svelte-11ps6r1").text
+    description = i.find("div", class_="snippet-description desktop-default-regular")
+    s_results.append({"url": url, "title": title, "description": description})
+  return s_results
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 from functions.google import g_search
 from functions.duck import duck_search
+from functions.brave import brave_search
 
 app = Flask(__name__)
 
@@ -12,21 +13,20 @@ def index():
         term = request.form["term"]
         g_result = g_search(term, 10, "us")
         duck_result = duck_search(term, 10, "wt-wt")
-        all_results = []
-        all_results.append(
-            {
-                "search": "Google",
-                "results": g_result
-            }
-        )
-        all_results.append(
-            {
-                "search": "DuckDuckGo",
-                "results": duck_result
-            }
-        )
+        brave_result = brave_search(term)
+        all_results = [{
+            "search": "Google",
+            "results": g_result
+        }, {
+            "search": "DuckDuckGo",
+            "results": duck_result
+        }, {
+            "search": "Brave",
+            "results": brave_result
+        }]
         return render_template("home.html", web_res=all_results, term=term)
     return render_template("home.html")
 
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
