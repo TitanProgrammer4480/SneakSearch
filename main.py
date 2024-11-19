9f4c56d4b13edffd66fe6e29a73d1294b94d4e50
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-19 07:53:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,25 @@ def index():
     if request.method == "POST":
         term = request.form["term"]
         g_box = request.form.get('google', "off")
+        d_box = request.form.get('duckduckgo', "off")
+        b_box = request.form.get('brave', "off")
         g_result = g_search(term, 10, "us")
         duck_result = duck_search(term, 10, "wt-wt")
         brave_result = brave_search(term)
         all_results = [{
             "search": "Google",
+            "status": g_box,
             "results": g_result
         }, {
             "search": "DuckDuckGo",
+            "status": d_box,
             "results": duck_result
         }, {
             "search": "Brave",
+            "status": b_box,
             "results": brave_result
         }]
-        return render_template("home.html", web_res=all_results, term=term, g_box=g_box)
+        return render_template("home.html", web_res=all_results, term=term)
     return render_template("home.html")
 
 
