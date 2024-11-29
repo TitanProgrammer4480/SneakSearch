13afe456c07751f25b2bfcf690fe0c6d572e09a2
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-11-29 11:47:08 +0000

    changed brave

--- functions/brave.py
+++ functions/brave.py
@@ -1,17 +1,7 @@
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup as bs
+from brave import Brave
 
-
-def brave_search(s_term):
-  session = HTMLSession()
-  s_results = []
-  results = session.get(f"https://search.brave.com/search?q={s_term}&offset=0")
-  results.html.render()
-  soup = bs(results.text, 'html.parser')
-  divs = soup.find_all("div", attrs={"data-type": "web"}, class_="snippet svelte-wmqx28")
-  for i in divs:
-    url = i.find("a", class_="h svelte-8pffig l1")["href"]
-    title = i.find("div", class_="sitename svelte-11ps6r1").text
-    description = i.find("div", class_="snippet-description desktop-default-regular")
-    s_results.append({"url": url, "title": title, "description": description})
-  return s_results
+def brave_search(s_term, s_num):
+  brave = Brave()
+  s_results = brave.search(q=s_term, count=s_num)
+  print(s_results.web_results)
+  return s_results.web_results
