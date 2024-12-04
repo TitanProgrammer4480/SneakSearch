cd9c35f7e1f5f255be7abc20a97d777c677c4aa0
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2024-12-04 15:11:16 +0000

    added login

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+from enum import unique
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql import func
 
 from functions.google import g_search
 from functions.duck import duck_search
 #from functions.brave import brave_search
 
-class Base(DeclarativeBase):
-  pass
 
-db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy()
 db.init_app(app)
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str]
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -69,20 +71,20 @@ def login():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        print(request.form["name"])
-        if request.form["name"] == "admin" and request.form[
-                "password"] == "admin":
-            session["username"] = request.form["name"]
-            return redirect(url_for("dashboard"))
-        print(hash(request.form["name"].encode()))
-        print(request.form["password"])
-        print(hash(request.form["password"].encode()))
+        user = User(username=hash(request.form["username"].encode()), email=hash(request.form["email"].encode()), password=hash(request.form["password"].encode()))
+        db.session.add(user)
+        db.session.commit()
+        session["username"] = request.form["username"]
+        return redirect(url_for("dashboard"))
     return render_template("signup.html")
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
     if "username" in session:
-        return render_template("dashboard.html")
+        print(session["username"])
+        print(User.query.all())
+        session_user = User.query.filter_by(username=session["username"]).all()
+        return render_template("dashboard.html", user=session_user)
     return "you are not logged in"
 
 
